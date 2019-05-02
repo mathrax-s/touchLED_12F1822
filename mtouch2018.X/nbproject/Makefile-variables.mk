222562f4d6837c3cdf84fcfa82aa308f757209c7
mathrax-m <shozo@mathrax.com>
2019-05-02 15:44:52 +0900

    xc8 ver2.05

---
+++ mtouch2018.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# free configuration
+CND_ARTIFACT_DIR_free=dist/free/production
+CND_ARTIFACT_NAME_free=mtouch2018.X.production.hex
+CND_ARTIFACT_PATH_free=dist/free/production/mtouch2018.X.production.hex
+CND_PACKAGE_DIR_free=${CND_DISTDIR}/free/package
+CND_PACKAGE_NAME_free=mtouch2018.x.tar
+CND_PACKAGE_PATH_free=${CND_DISTDIR}/free/package/mtouch2018.x.tar
